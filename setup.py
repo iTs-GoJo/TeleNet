98fb2196663a47e7e3908f88eca58cee1e7a3c2b
Ali-jafari <mmdddpdd@gmail.com>
2026-08-26 17:07:59 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup, find_packages
+
 setup(
     name="telenet",
-    version="2.7.0",
+    version="3.0.0",
     packages=find_packages(),
     install_requires=["aiohttp>=3.9"],
+    python_requires=">=3.9",
+    author="Ali Jafari",
+    author_email="thealiapi@gmail.com",
+    description="TeleNet: Async Telegram Bot API framework",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: AsyncIO",
+        "Topic :: Communications :: Chat",
+    ],
 )
